567974074132802a8daaa036e6869d2d0e7b47c6
Seenshade <Seenshade.sb@gmail.com>
2019-06-09 18:12:55 +0300

    Add files via upload

---
+++ Practical_task_№7/task1.c
@@ -0,0 +1,8 @@
+void incrementHeapVariable() {
+	int *p = (int*) malloc(sizeof(int));
+	*p = 10;
+	printf("Address: %p\np= %d\n", p, *p);
+	(*p)++;
+	printf("Address: %p\np= %d\n", p, *p);
+	free(p);
+}
